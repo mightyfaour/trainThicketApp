ab29dbb91447017a2db1546b5a681ca3e4bb74be
techub <tech4phil@outlook.com>
2022-10-17 13:43:29 +0100

    Initial commit

---
+++ src/main/java/com/example/trainthicketapp/TrainThicketAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.trainthicketapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainThicketAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrainThicketAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/trainthicketapp/TrainThicketAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.trainthicketapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainThicketAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
