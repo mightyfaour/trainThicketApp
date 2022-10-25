f4a82f50d8daef68957a82dbd9ab2d09df7a582d
techub <tech4phil@outlook.com>
2022-10-25 17:54:48 +0100

    25rd, october, 2022

--- src/main/java/com/example/trainthicketapp/model/data/PassengerInfo.java
+++ src/main/java/com/example/trainthicketapp/model/data/PassengerInfo.java
@@ -14,6 +14,7 @@ import java.util.Date;
 @Entity
 @Table(name="PASSENGER_INFO")
 public class PassengerInfo {
+
     @Id
     @GeneratedValue (strategy = GenerationType.IDENTITY)
     private Long Id;
